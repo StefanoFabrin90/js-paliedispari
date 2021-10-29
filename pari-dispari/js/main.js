6c87692034ab22062d2cb44e144ca87820473c55
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-10-29 15:33:23 +0200

    es 2

--- pari-dispari/js/main.js
+++ pari-dispari/js/main.js
@@ -1,1 +1,49 @@
-console.log('js ok');
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+
+// GAMER
+let EvenOdd = prompt ('Inserire pari o dispari');
+
+if ( (EvenOdd !== 'pari') && (EvenOdd !== 'dispari') ) {
+    alert('Inserimento non valido, Assegnazione automatica: pari')
+    EvenOdd = 'pari';
+}
+console.log(EvenOdd);
+
+let gamer = parseInt( prompt('Inserire un numero da 1 a 5') );
+
+if ( (gamer < 1) || (gamer > 5) || isNaN(gamer) ) {
+    alert('Inserimento non valido, Assegnazione automatica : 3')
+    gamer = 3;
+}
+console.log(gamer);
+
+
+
+// PC
+for (let i = 0; 1 < 5; i ++){
+
+    const randNumber = NumberRandom (1, 5);
+    console.log(randNumber);
+}
+
+
+
+
+
+
+
+
+
+// FUNZIONE
+
+function NumberRandom (min, max) {
+    const rand = Math.floor(Math.random()*(max-min +1)) + min;
+
+    return rand;
+}
